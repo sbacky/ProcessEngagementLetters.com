2483f3134d46851afb68002636c4ae710043cba0
Neil Crum <nhc.crum@outlook.com>
2023-12-24 17:04:30 -0700

    Add GNU GPL version 3 license

--- frontend/scripts/api.js
+++ frontend/scripts/api.js
@@ -1,4 +1,16 @@
-// ./frontend/scripts/api.js
+/* Copyright (C) 2023 - Neil Crum (nhc.crum@outlook.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License */
 import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
 
 /**
